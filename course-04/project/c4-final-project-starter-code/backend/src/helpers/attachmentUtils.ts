9ac28808f271b1b20a4b4b961677f317c6af66e0
NfoTECH <fortunenwachukwu@gmail.com>
2023-01-18 22:00:47 +0100

    copy the api id to the config.ts file for frontend interaction

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -4,9 +4,9 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
-const s3BucketName = process.env.ATTACHMENTS_S3_BUCKET
+const s3BucketName = process.env.ATTACHMENT_S3_BUCKET
 // const urlExpiration = process.env.SIGNED_URL_EXPIRATION
-const urlExpiration = 300
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export class AttachmentUtils {
     constructor(
